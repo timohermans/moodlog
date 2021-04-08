5af6e0d9f7ba1bd72c8e956356fb058a78408de9
Timo Hermans <tmhermans@gmail.com>
2021-04-08 21:29:55 +0200

    Start styling layout of dashboard

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -1,9 +1,15 @@
 describe('dashboard', () => {
-    it('link to several utility pages', () => {
+    it('links to several utility pages', () => {
         cy.visit('/dashboard');
 
-        cy.get('[data-cy=triple-column]')
-            .should('have.attr', 'href')
-            .and('equal', '/triple-column');
+        cy.get('[data-cy=user-picture]').should('exist');
+        cy.contains('Timo Tester');
+
+        cy.get('[data-cy=nav-dashboard]').should('have.attr', 'href').and('equal', '/dashboard');
+        cy.get('[data-cy=nav-triple-column]').should('have.attr', 'href').and('equal', '/triple-column');
+
+        cy.get('[data-cy=nav-logout]').click();
+        cy.url().should('equal', `${Cypress.config().baseUrl}/`);
+        cy.get('[data-cy=login]').should('exist');
     });
 })
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,24 @@
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import React from 'react'
 import { guardAuthenticated } from './utils/authenticationGuard'
 
 interface Props {}
 
 const dashboard = (props: Props) => {
-  return <div>hello dashboard</div>
+  return (
+    <div className="flex h-screen bg-blue-900">
+      <Head>
+        <title>Dashboard | Moodlog</title>
+      </Head>
+
+      <div className="w-1/12 "></div>
+
+      <div className="p-2 w-full">
+        <main className="bg-white rounded-3xl h-full w-full"></main>
+      </div>
+    </div>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
