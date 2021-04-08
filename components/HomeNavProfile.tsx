5706072ec7765678c4f500866d7777aa3d762469
Timo Hermans <tmhermans@gmail.com>
2021-04-08 12:33:03 +0200

    Immediately go to keycloak instead of a intermediary login page

--- components/HomeNavProfile.tsx
+++ components/HomeNavProfile.tsx
@@ -1,25 +1,23 @@
 import { signIn, signOut, useSession } from 'next-auth/client'
-import { useRouter } from 'next/dist/client/router'
+import Link from 'next/link'
 
 interface Props {}
 
-const NavProfile = (props: Props) => {
+const HomeNavProfile = (props: Props) => {
   const [session, isLoading] = useSession()
-  const { route, push } = useRouter()
-
-  console.log(route)
 
   if (isLoading) return null
 
-  if (session && route === '/') {
-    return push('/dashboard')
-  }
-
   if (session) {
     return (
-      <div className="flex items-center justify-end md:flex-1">
-        Welcome, {session.user.name}
-      </div>
+      <Link href="/dashboard" passHref={true}>
+        <a
+          href="#"
+          className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow text-base font-medium text-white bg-blue-400 hover:bg-blue-500 cursor-pointer"
+        >
+          Go to dashboard, {session.user.name}
+        </a>
+      </Link>
     )
   }
 
@@ -27,7 +25,7 @@ const NavProfile = (props: Props) => {
     <div className="flex items-center justify-end md:flex-1">
       <a
         data-cy="login"
-        onClick={() => signIn()}
+        onClick={() => signIn('keycloak')}
         className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow text-base font-medium text-white bg-blue-400 hover:bg-blue-500 cursor-pointer"
       >
         Sign in
@@ -36,4 +34,4 @@ const NavProfile = (props: Props) => {
   )
 }
 
-export default NavProfile
+export default HomeNavProfile
